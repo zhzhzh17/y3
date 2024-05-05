ed57e8caa3b053bdb4cf8dbc2641e990dcfa40e2
yyc <1103045804@qq.com>
2024-05-06 04:17:26 +0800

    init

---
+++ custom/folder_info/custom_res_model_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'KXpS4Qo9Ee+MSAD-UyqzDR', '\xe5\x9c\xba\xe6\x99\xaf')], 'd': {}}
